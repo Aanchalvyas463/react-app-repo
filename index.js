c8ab13d25d5218362e0864a61d649e4130a58ea1
Aanchalvyas463 <112643033+Aanchalvyas463@users.noreply.github.com>
2024-02-22 18:59:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM =require('react-dom');
+
+ReactDOM.render(<h1> Hello Everyone!!
+                This is my React App Website..
+</h1>,
+    document.getElementById('root'));
